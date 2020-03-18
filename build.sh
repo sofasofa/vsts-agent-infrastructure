79801c6bfbd89c786a761e9422b48449cded8ccf
sofasofa <sofiene.hamoudi@gmail.com>
2020-03-19 00:11:53 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t vsts-agent-infrastructure --build-arg VCS_REF="git rev-parse --short HEAD" .
-docker tag vsts-agent-infrastructure lenisha/vsts-agent-infrastructure 
-docker push lenisha/vsts-agent-infrastructure 
+docker tag vsts-agent-infrastructure sofasofa/vsts-agent-infrastructure 
+docker push sofasofa/vsts-agent-infrastructure 
